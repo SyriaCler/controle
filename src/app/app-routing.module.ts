99fea597b44895a1f206b85baa29cb8f1d2ae18a
SyriaCler <syriacler@gmail.com>
2020-01-06 00:42:22 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
+import { ContentComponent } from './content/content.component'
+import { LoginComponent } from './login/login.component';
+import { ProjetosComponent } from './projetos/projetos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: ContentComponent},
+  { path: '**', redirectTo: "home"},
+  { path: 'login', component: LoginComponent},
+  { path: 'projetos', component: ProjetosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const OUTRO_ROUTING = RouterModule.forRoot(routes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+// rotas
+import { OUTRO_ROUTING } from "./app-routing.module"
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { ProjetosComponent } from './projetos/projetos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    FooterComponent,
+    ContentComponent,
+    ProjetosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OUTRO_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
